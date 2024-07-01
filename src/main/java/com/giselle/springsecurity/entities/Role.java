6dfcdbbbdc59f99c09ab54cdbc4c0613015d6c80
Giselle Souza <giselle_souza97@yahoo.com.br>
2024-07-01 19:46:43 -0300

    entity relationship mapping

--- src/main/java/com/giselle/springsecurity/entities/Role.java
+++ src/main/java/com/giselle/springsecurity/entities/Role.java
@@ -1,17 +1,23 @@
 package com.giselle.springsecurity.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_roles")
+@Getter
+@Setter
 public class Role {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "role_id")
 	private Long roleId;
 	
 	private String nome;
--- src/main/java/com/giselle/springsecurity/entities/Tweet.java
+++ src/main/java/com/giselle/springsecurity/entities/Tweet.java
@@ -4,19 +4,24 @@ import java.time.Instant;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_tweet")
+@Getter
+@Setter
 public class Tweet {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "tweet_id")
 	private Long tweetId;
 	
 	private User user;
